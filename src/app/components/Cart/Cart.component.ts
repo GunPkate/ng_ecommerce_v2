0207d9a56640b77ae6d3b650cd34f2e8abc8f375
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-11-09 19:47:59 +0700

    cart & productlist components

---
+++ src/app/components/Cart/Cart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-Cart',
+  standalone: true,
+  imports: [],
+  templateUrl: './Cart.component.html',
+  styleUrl: './Cart.component.css'
+})
+export class CartComponent {
+
+}
---
+++ src/app/components/ProductList/ProductList.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ProductList',
+  standalone: true,
+  imports: [],
+  templateUrl: './ProductList.component.html',
+  styleUrl: './ProductList.component.css'
+})
+export class ProductListComponent {
+
+}
