a7a78fa80300845f53b1a149e4f8ce09a4049a3b
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-11-09 20:36:55 +0700

    set page product & cart

--- src/app/components/Cart/Cart.component.ts
+++ src/app/components/Cart/Cart.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { CartItem } from 'src/app/interface/CartItem';
 
 @Component({
   selector: 'app-Cart',
   templateUrl: './Cart.component.html',
   styleUrls: ['./Cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  cartItems: CartItem[] = [];
+  total: number = 0;
 
+
+
+  ngOnInit(): void {
+
+  }
+
+  removeItem(productId: number): void {
+
+  }
 }
--- src/app/components/ProductList/ProductList.component.ts
+++ src/app/components/ProductList/ProductList.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/interface/Product';
 
 @Component({
   selector: 'app-ProductList',
   templateUrl: './ProductList.component.html',
   styleUrls: ['./ProductList.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(
+
+  ) {}
+
+  ngOnInit(): void {
+   
+  }
+
+  addToCart(product: Product): void {
+ 
+  }
 }
+
